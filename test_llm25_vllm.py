1a5a125a3cf618bf6c2cb9a982cbd54fcc4ce633
jieun9999 <kindjjee@gmail.com>
2025-02-14 00:56:21 +0000

    AWQ 양자화 적용전

--- test_llm25_vllm.py
+++ test_llm25_vllm.py
@@ -1,27 +1,8 @@
 from set_prompt import get_prompt
-from vllm import LLM
-from transformers import AutoTokenizer, GPT2LMHeadModel, BitsAndBytesConfig, AutoModelForCausalLM
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
 import pandas as pd
 import copy
-
-# 주어진 모델 이름에 따라 vLLM을 로드하는 함수
-quantization_config = BitsAndBytesConfig(load_in_8bit=True) #⭐vllm 적용 전 임시 추가
-
-def load_llm(model_name):
-    #KoGPT만 예제 프롬프트에서 모델 로드 코드가 달랐어서 일단 if문으로 감싸둘게요.
-    if model_name == "skt/kogpt2-base-v2" : 
-        model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2', quantization_config=quantization_config)
-        return model
-    
-    llm = LLM(model_name=model_name, quantization_config=quantization_config) #⭐vllm 적용 전 임시 추가
-    return llm
-
-# 주어진 모델 이름에 따라 Hugging Face 토크나이저를 로드하는 함수
-def load_tokenizer(model_name):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.eos_token = tokenizer.eos_token  # 패딩 토큰 설정
-    tokenizer.padding_side = 'right'  # 패딩 방향 설정
-    return tokenizer
 
 model_names = [
     "NCSOFT/Llama-VARCO-8B-Instruct",
@@ -51,6 +32,19 @@ model_names = [
     "skt/kogpt2-base-v2",
 ]
 
+# 주어진 모델 이름에 따라 vLLM을 로드하는 함수
+def load_llm(model_name):
+    llm = LLM(model_name=model_name) 
+    return llm
+
+# 주어진 모델 이름에 따라 Hugging Face 토크나이저를 로드하는 함수
+def load_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.eos_token = tokenizer.eos_token  # 패딩 토큰 설정
+    tokenizer.padding_side = 'right'  # 패딩 방향 설정
+    return tokenizer
+
+# 특정 토큰들을 정지 조건(Stop Tokens)으로 설정
 stops =[ #        
                 "<|start_header_id|>", # Llama. 출력이 멈춰야 하는 토큰. (varco만 할 때 지시문이 나왔던 건 이것때문일지도..)
                 "<|end_header_id|>",
@@ -77,27 +71,18 @@ file_path = '/workspace/test3_llm25.csv'
 # CSV 파일 읽기
 df = pd.read_csv(file_path)
 
+
 for model_name in model_names:
+
+    # 모델 이름에 따라 적합한 일상 대화 프롬프트(casual_prompt)와 감정 공감 프롬프트(empathic_prompt)를 생성
     casual_prompt, empathic_prompt, formatted = get_prompt(model_name)
-    
-    prompt_list = [casual_prompt, empathic_prompt]
+    prompt_list = [casual_prompt, empathic_prompt] #  두 가지 프롬프트를 리스트로 묶음
 
     for idx, prompt in enumerate(prompt_list):
-         # vLLM 및 Hugging Face 토크나이저 로드
-        # llm = load_llm(model_name)
+        llm = load_llm(model_name)
         tokenizer = load_tokenizer(model_name)
 
-        if model_name == "" : 
-            model = GPT2LMHeadModel.from_pretrained(
-                model_name,
-                quantization_config=quantization_config
-            )
-        else : 
-            model = AutoModelForCausalLM.from_pretrained(
-                model_name,
-                quantization_config=quantization_config
-            )
-
+        # stops 리스트를 복사하여 모델에 따라 추가적인 종료 토큰을 설정
         stops_copy = copy.deepcopy(stops)
         if model_name == "THUDM/glm-4-9b-chat" or model_name == "4n3mone/glm-4-ko-9b-chat" : 
             stops_copy.extend(["<|system|>", "<|user|>", "<|assistant|>"]) #리스트를 추가하려면 append 대신 extend를 사용
@@ -106,40 +91,44 @@ for model_name in model_names:
         elif model_name == "yanolja/Bookworm-10.7B-v0.4-DPO" :
             stops_copy.append("</s>")
 
-
         if formatted : 
-            inputs = tokenizer.apply_chat_template(prompt, return_tensors="pt", padding=True, truncation=True).to(model.device)
+            inputs = tokenizer.apply_chat_template(prompt, return_tensors="pt", padding=True, truncation=True)
         else : 
-            inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).to(model.device)
+            # 대화형 구조의 데이터 형식을 입력받지 못하거나, 그런 형식을 요구하지 않기 때문에 단순한 텍스트 입력으로 처리
+            inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True)
 
+        
         # attention_mask 설정
         attention_mask = inputs['input_ids'] != tokenizer.pad_token_id
 
         # 종료 토큰 ID 설정
         eos_token_id = [
-                tokenizer.eos_token_id
+            tokenizer.eos_token_id
         ]
 
+        # stop_copy 리스트에 있는 특정 토큰(종료조건으로 사용할 토큰)을 토크나이저를 통해 해당토큰의 id로 변환한 다음,
+        # 이를 eos_token_id 리스트에 추가
         for stop in stops_copy : 
             token_id = tokenizer.convert_tokens_to_ids(stop)
             eos_token_id.append(token_id)
-       
-        # 결과 출력
-        input_length = inputs['input_ids'].size(1)
-        outputs = model.generate(
-            **inputs,
-            attention_mask=attention_mask,  # attention_mask 추가
-            eos_token_id=eos_token_id,
-            max_new_tokens= 100,
-            no_repeat_ngram_size= 3, # 값이 커질수록 더 긴 반복 구문을 방지하므로, 텍스트가 더 다양해질 가능성이 높습니다.
-            repetition_penalty=2.0,
-            pad_token_id=tokenizer.eos_token_id
-        )        
-         # 출력 결과 디코딩 
-        # 모델이 새롭게 생성한 텍스트만 선택
-        generated_text = tokenizer.decode(outputs[0][input_length:], skip_special_tokens=True)
 
-        ###
+
+        ## 결과 출력
+
+        # SamplingParams 설정
+        sampling_params = SamplingParams(
+        stop_token_ids=eos_token_id, #생성 중지하는 토큰 목록
+        max_tokens=100,  
+        repetition_penalty=2.0,  # 반복 페널티
+        )
+
+        # 텍스트 생성
+        outputs = llm.generate(prompt, sampling_params)
+        # outputs 구조 확인
+        print(outputs)
+
+        input_length = inputs['input_ids'].size(1)  # 입력 텍스트 길이 계산
+        generated_text = outputs[0].outputs[0].text[input_length:] # 모델이 새롭게 생성한 텍스트만 선택
 
         # model_name이 포함된 행 찾기
         rows_to_update = df[df.iloc[:, 0] == model_name]
@@ -161,6 +150,3 @@ for model_name in model_names:
         with open('test3_llm25.txt', 'a', encoding='utf-8') as f:
             f.write(output_text + '\n')
         print(output_text)
-
-        
-
